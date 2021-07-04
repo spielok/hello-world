f77c9aa0f78831b514053ffd728e037cdf22c0dc
spielok <86842633+spielok@users.noreply.github.com>
2021-07-04 13:17:59 +0200

    Update HelloWorld.java
    fix typo

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    System.our.println("Hello GitHub!");
+    System.out.println("Hello GitHub!");
   }
 }
